bb2a33d7fbfc62cb2d3b1f2d7ff6e46be0233b0c
sergei.baikin <sergei.baikin@fotograf.de>
2023-06-01 15:40:15 +0200

    Update symfony/event-dispatcher

--- library/Xi/Sms/Event/SmsMessageEvent.php
+++ library/Xi/Sms/Event/SmsMessageEvent.php
@@ -9,7 +9,7 @@
 
 namespace Xi\Sms\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 use Xi\Sms\SmsMessage;
 
 class SmsMessageEvent extends Event
